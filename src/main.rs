e08909825c5132a07076a9b4c27344c06176db41
yds12 <yds12@protonmail.com>
2022-10-14 01:19:41 +0200

    smoothen the random map

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
-const SCREEN_SIZE: (u32, u32) = (307, 167);
-const TILE_SIZE: (u32, u32) = (4, 4);
+const SCREEN_SIZE: (usize, usize) = (307, 167);
+const TILE_SIZE: (usize, usize) = (4, 4);
 
 const BG_COLOR: (f32, f32, f32) = (1.0, 0.0, 1.0);
 
+const SMOOTHING: usize = 30;
+
+const fn get_coord(index: usize) -> (usize, usize) {
+    (index % SCREEN_SIZE.0, index / SCREEN_SIZE.0)
+}
+
+const fn get_index(x: usize, y: usize) -> usize {
+    y * SCREEN_SIZE.0 + x
+}
+
+fn get_neighbor_coords(x: usize, y: usize) -> Vec<(usize, usize)> {
+    let mut neighs = Vec::new();
+
+    if x > 0 {
+        neighs.push((x - 1, y));
+    }
+    if x > 0 && y > 0 {
+        neighs.push((x - 1, y - 1));
+    }
+    if x > 0 && y < SCREEN_SIZE.1 - 1 {
+        neighs.push((x - 1, y + 1));
+    }
+    if y > 0 {
+        neighs.push((x, y - 1));
+    }
+    if x < SCREEN_SIZE.0 - 1 && y > 0 {
+        neighs.push((x + 1, y - 1));
+    }
+    if x < SCREEN_SIZE.0 - 1 {
+        neighs.push((x + 1, y));
+    }
+    if x < SCREEN_SIZE.0 - 1 && y < SCREEN_SIZE.1 - 1 {
+        neighs.push((x + 1, y + 1));
+    }
+    if y < SCREEN_SIZE.1 - 1 {
+        neighs.push((x, y + 1));
+    }
+
+    neighs
+}
+
+#[derive(Clone, Copy, Debug)]
 enum Terrain {
     Grass,
     Resource,
@@ -30,7 +72,7 @@ struct GameState {
 impl GameState {
     pub fn new() -> Self {
         use rand::prelude::*;
-        Self {
+        let mut state = Self {
             map: (0..SCREEN_SIZE.0 * SCREEN_SIZE.1)
                 .map(|_| MapCell {
                     terrain: match rand::thread_rng().gen_range(0..3) {
@@ -41,7 +83,66 @@ impl GameState {
                     },
                 })
                 .collect(),
+        };
+
+        for _ in 0..SMOOTHING {
+            state.smoothen();
         }
+
+        state
+    }
+
+    fn smoothen(&mut self) {
+        use rand::prelude::*;
+
+        let mut commons = Vec::new();
+        let len = self.map.len();
+
+        for index in 0..len {
+            let common_vec = self.most_common_neighbor(index);
+            let ind = rand::thread_rng().gen_range(0..common_vec.len());
+            commons.push(common_vec[ind]);
+        }
+
+        for index in 0..len {
+            self.map[index].terrain = commons[index];
+        }
+    }
+
+    fn most_common_neighbor(&self, index: usize) -> Vec<Terrain> {
+        let neighs = self.get_neighbors(index);
+        let mut counts = (0, 0, 0);
+
+        for neigh in neighs {
+            match neigh {
+                Terrain::Grass => counts.0 += 1,
+                Terrain::Resource => counts.1 += 1,
+                Terrain::Water => counts.2 += 1
+            }
+        }
+
+        // TODO: improve this
+        match counts {
+            (g, r, w) if w == g && w == r => vec![Terrain::Water, Terrain::Resource, Terrain::Grass],
+            (g, r, w) if w > g && w == r => vec![Terrain::Water, Terrain::Resource],
+            (g, r, w) if w > r && w == g => vec![Terrain::Water, Terrain::Grass],
+            (g, r, w) if w > g && w > r => vec![Terrain::Water],
+            (g, r, _) if r == g => vec![Terrain::Grass, Terrain::Resource],
+            (g, r, _) if r > g => vec![Terrain::Resource],
+            _ => vec![Terrain::Grass],
+        }
+    }
+
+    fn get_neighbors(&self, index: usize) -> Vec<Terrain> {
+        let mut terrs = Vec::new();
+        let pos = get_coord(index);
+        let neigh_coords = get_neighbor_coords(pos.0, pos.1);
+
+        for (x, y) in neigh_coords {
+            terrs.push(self.map[get_index(x, y)].terrain);
+        }
+
+        terrs
     }
 }
 
@@ -58,7 +159,7 @@ impl ggez::event::EventHandler for GameState {
         );
 
         for (index, cell) in self.map.iter().enumerate() {
-            let pos = (index as u32 % SCREEN_SIZE.0, index as u32 / SCREEN_SIZE.0);
+            let pos = get_coord(index);
             let color = cell.color();
             canvas.draw(
                 &ggez::graphics::Quad,
@@ -87,9 +188,9 @@ impl ggez::event::EventHandler for GameState {
 
 fn main() -> ggez::GameResult {
     // Here we use a ContextBuilder to setup metadata about our game. First the title and author
-    let (ctx, events_loop) = ggez::ContextBuilder::new("snake", "Gray Olson")
+    let (ctx, events_loop) = ggez::ContextBuilder::new("SIM", "yds12")
         // Next we set up the window. This title will be displayed in the title bar of the window.
-        .window_setup(ggez::conf::WindowSetup::default().title("Snake!"))
+        .window_setup(ggez::conf::WindowSetup::default().title("SIM"))
         // Now we get to set the size of the window, which we use our SCREEN_SIZE constant from earlier to help with
         .window_mode(ggez::conf::WindowMode::default().dimensions(
             (SCREEN_SIZE.0 * TILE_SIZE.0) as f32,
